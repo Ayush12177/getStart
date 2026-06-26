052d8c8ff6598aa3146ef5210204932e99fa96a7
Ayush Gupta <ayushgupta96396@gmail.com>
2026-06-26 12:09:42 +0530

    Time: 8 ms (31.01%), Space: 44.2 MB (82.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -13,9 +13,6 @@ class Solution {
 
         for(int i=0; i<s.length(); i++){
             freq[s.charAt(i)-'a']++;
-        }
-
-        for(int i=0; i<t.length(); i++){
             freq[t.charAt(i)-'a']--;
         }
 
