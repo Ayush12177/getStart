9381afdd1c1409971a94734363467ed58b4493b2
Ayush Gupta <ayushgupta96396@gmail.com>
2026-07-07 12:40:40 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,15 +1,31 @@
 class Solution {
+
+    int[] dp= new int[31];
+
     public int fib(int n) {
-        if(n==0 || n==1){
+        // if (n == 0 || n == 1) {
+        //     return n;
+        // }
+
+        //  int fn1= fib(n-1);
+        //  int fn2= fib(n-2);
+        //  int fn= fn1 + fn2;
+        //  return fn;
+
+        // return fib(n - 1) + fib(n - 2);
+
+        if(n <= 1){
             return n;
         }
 
-        // int fn1= fib(n-1);
-        // int fn2= fib(n-2);
-        // int fn= fn1 + fn2;
-        // return fn;
+        if(dp[n] != 0){
+            return dp[n];
+        }
 
-        return fib(n-1)+ fib(n-2);
+        dp[n]=fib(n-1)+fib(n-2);
+
+        return dp[n];
         
+
     }
 }
